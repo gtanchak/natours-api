a08791955cba2d381784d934b59990b1ac834acb
ghanshyamtanchak <ghtanchak77@gmail.com>
2023-04-15 11:15:21 +0530

    add crud openration with express

--- app.js
+++ app.js
@@ -43,6 +43,65 @@ app.post('/api/v1/tours', (req, res) => {
   );
 });
 
+// GET TOUR
+app.get('/api/v1/tours/:id', (req, res) => {
+  const id = req.params.id * 1;
+  const tour = tours.find((ele) => ele.id === id);
+
+  if(!tour) {
+    return res.status(404).json({
+      status:'fail',
+      message:"Invalid Id"
+    })
+  }
+
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tour,
+    },
+  });
+});
+
+
+// UPDATE TOUR
+app.patch('/api/v1/tours/:id', (req, res) => {
+  const id = req.params.id * 1;
+  const tour = tours.find((ele) => ele.id === id);
+
+  if(req.params.id > tours.length) {
+    return res.status(404).json({
+      status:'fail',
+      message:"Invalid Id"
+    })
+  }
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<Updated tour here>',
+    },
+  });
+})
+
+
+// DLETE TOUR
+app.delete('/api/v1/tours/:id', (req, res) => {
+  const id = req.params.id * 1;
+  const tour = tours.find((ele) => ele.id === id);
+
+  if(req.params.id > tours.length) {
+    return res.status(404).json({
+      status:'fail',
+      message:"Invalid Id"
+    })
+  }
+  res.status(204).json({
+    status: 'success',
+    data: null
+  });
+})
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
