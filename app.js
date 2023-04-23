9f90ceb686eced7a7bf6ee012cfcf1fb440c1bab
ghanshyamtanchak <ghtanchak77@gmail.com>
2023-04-23 17:24:49 +0530

    feat: setup eslint and prettier

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
 const express = require('express');
 
-const moragn = require('morgan')
+const moragn = require('morgan');
 
-const tourRouter = require('./routes/tourRoutes')
-const userRouter = require('./routes/userRoutes')
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-app.use(moragn('dev'))
+if (process.env.NODE_ENV === 'development') {
+  app.use(moragn('dev'));
+}
 app.use(express.json());
-app.use(express.static(`${__dirname}/public`))
+app.use(express.static(`${__dirname}/public`));
 
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
 
-app.use('/api/v1/tours', tourRouter)
-app.use('/api/v1/users', userRouter)
-
-module.exports = app
+module.exports = app;
--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,86 +1,85 @@
-const fs = require('fs')
+/* eslint-disable node/no-unsupported-features/es-syntax */
+const fs = require('fs');
 
 const tours = JSON.parse(
-    fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-  );
+  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
+);
 
-exports.chekID = (req,res,next, val) => {
-  if(req.params.id > tours.length) {
+exports.chekID = (req, res, next, val) => {
+  if (req.params.id > tours.length) {
     return res.status(404).json({
-      status:'fail',
-      message:"Invalid Id"
-    })
+      status: 'fail',
+      message: 'Invalid Id',
+    });
   }
-  next()
-}
+  next();
+};
 
 exports.checkBody = (req, res, next) => {
-  console.log(req.body)
-  if(!req.body.name || !req.body.price) {
+  if (!req.body.name || !req.body.price) {
     return res.status(400).json({
-      status:'fail',
-      message:"Missing Price or Name"
-    })
+      status: 'fail',
+      message: 'Missing Price or Name',
+    });
   }
-  next()
-}
-  
+  next();
+};
+
 exports.getAllTours = (req, res) => {
-    res.status(200).json({
-      status: 'success',
-      results: tours.length,
-      data: {
-        tours,
-      },
-    });
-  }
-  
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+};
+
 exports.createTour = (req, res) => {
-    const newId = tours[tours.length - 1].id + 1;
-    const newTour = Object.assign({ id: newId }, req.body);
-  
-    tours.push(newTour);
-  
-    fs.writeFile(
-      `${__dirname}/../dev-data/data/tours-simple.json`,
-      JSON.stringify(tours),
-      (err) => {
-        res.status(201).json({
-          status: 'success',
-          data: {
-            tour: newTour,
-          },
-        });
-      }
-    );
-  }
-  
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = { id: newId, ...req.body };
+
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/../dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    () => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+};
+
 exports.getTour = (req, res) => {
-    const id = req.params.id * 1;
-    const tour = tours.find((ele) => ele.id === id);
-  
-    res.status(200).json({
-      status: 'success',
-      results: tours.length,
-      data: {
-        tour,
-      },
-    });
-  }
-  
+  const id = req.params.id * 1;
+  const tour = tours.find((ele) => ele.id === id);
+
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tour,
+    },
+  });
+};
+
 exports.updateTour = (req, res) => {
-    res.status(200).json({
-      status: 'success',
-      data: {
-        tour: '<Updated tour here>',
-      },
-    });
-  }
-  
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<Updated tour here>',
+    },
+  });
+};
+
 exports.deleteTour = (req, res) => {
-    
-    res.status(204).json({
-      status: 'success',
-      data: null
-    });
-  }
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+};
