ec329bc616c50cfe6118b9dc8f26c36406b6f905
Ghanshyam Tanchak <81280194+ghanshyamtanchak@users.noreply.github.com>
2023-05-09 18:23:35 +0530

    working on mongoose connection

--- server.js
+++ server.js
@@ -1,7 +1,36 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const app = require('./app');
 
 dotenv.config({ path: './config.env' });
-const app = require('./app');
+
+const DB = process.env.DATABASE;
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('DB connection successfully'));
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tours must have a name'],
+    uniq: true,
+  },
+  rating: {
+    type: Number,
+    default: 5.12,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tours must have a price'],
+  },
+});
+
+console.log(tourSchema);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
