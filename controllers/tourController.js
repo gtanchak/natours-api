8c6b3abce3b9311574390cbfdf74a318e1afa131
Ghanshyam Tanchak <81280194+ghanshyamtanchak@users.noreply.github.com>
2023-05-24 12:32:30 +0530

    removing unused code

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,14 +1,5 @@
 // const Tour = require('../models/tourModel');
 
-exports.checkBody = (req, res, next) => {
-  if (!req.body.name || !req.body.price) {
-    return res.status(400).json({
-      status: 'fail',
-      message: 'Missing Price or Name',
-    });
-  }
-  next();
-};
 
 exports.getAllTours = (req, res) => {
   res.status(200).json({
@@ -20,7 +11,25 @@ exports.getAllTours = (req, res) => {
   });
 };
 
-exports.createTour = (req, res) => {
+exports.createTour = async (req, res) => {
+
+  try {
+    const newTour = await Tour.create(req.body);
+    
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour,
+      },
+    });
+    
+  } catch (error) {
+    res.status(400).json({
+      status:'fail',
+      message:"Invalid Data"
+    })
+  }
+
   res.status(201).json({
     status: 'success',
     // data: {
