daab6677129fea9be9aea0c600fd6636ac1d8b63
ghanshyamtanchak <ghtanchak77@gmail.com>
2023-05-27 11:29:38 +0530

    added delete tour with database

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -35,16 +35,21 @@ exports.createTour = async (req, res) => {
   }
 };
 
-exports.getTour = (req, res) => {
-  // const id = req.params.id * 1;
-  // const tour = tours.find((ele) => ele.id === id);
-  // res.status(200).json({
-  //   status: 'success',
-  //   results: tours.length,
-  //   data: {
-  //     tour,
-  //   },
-  // });
+exports.getTour = async (req, res) => {
+  try {
+    const tour = await Tour.findById(req.params.id);
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      message: error,
+    });
+  }
 };
 
 exports.updateTour = async (req, res) => {
@@ -69,13 +74,13 @@ exports.updateTour = async (req, res) => {
 
 exports.deleteTour = async (req, res) => {
   try {
-    const tour = await Tour.deleteTour(req.params.id);
-    res.status(204).json({
+    await Tour.findByIdAndRemove(req.params.id);
+    res.status(200).json({
       status: 'success',
-      data: tour,
+      data: null,
     });
   } catch (error) {
-    res.status(400).json({
+    res.status(404).json({
       status: 'fail',
       message: error,
     });
