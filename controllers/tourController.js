42745f9c4fa4d30841ac8b320a4fc21a9f6ff5dc
ghanshyamtanchak <ghtanchak77@gmail.com>
2023-05-27 11:11:24 +0530

    fix connection error and update create tour route

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,41 +1,38 @@
-// const Tour = require('../models/tourModel');
+const Tour = require('../models/tourModel');
 
-
-exports.getAllTours = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    // results: tours.length,
-    // data: {
-    //   tours,
-    // },
-  });
+exports.getAllTours = async (req, res) => {
+  try {
+    const tours = await Tour.find();
+    res.status(200).json({
+      status: 'success',
+      results: tours.length,
+      data: {
+        tours,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      message: error,
+    });
+  }
 };
 
 exports.createTour = async (req, res) => {
-
   try {
     const newTour = await Tour.create(req.body);
-    
     res.status(201).json({
       status: 'success',
       data: {
         tour: newTour,
       },
     });
-    
   } catch (error) {
     res.status(400).json({
-      status:'fail',
-      message:"Invalid Data"
-    })
+      status: 'fail',
+      message: error,
+    });
   }
-
-  res.status(201).json({
-    status: 'success',
-    // data: {
-    //   tour: newTour,
-    // },
-  });
 };
 
 exports.getTour = (req, res) => {
@@ -53,27 +50,34 @@ exports.getTour = (req, res) => {
 exports.updateTour = async (req, res) => {
   try {
     const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
-      new:true,
-      runValidators:true
-    })
+      new: true,
+      runValidators: true,
+    });
     res.status(200).json({
       status: 'success',
       data: {
-        tour
+        tour,
       },
     });
-
   } catch (error) {
     res.status(400).json({
-      status:'fail',
-      message:error
-    })
+      status: 'fail',
+      message: error,
+    });
   }
 };
 
-exports.deleteTour = (req, res) => {
-  res.status(204).json({
-    status: 'success',
-    data: null,
-  });
+exports.deleteTour = async (req, res) => {
+  try {
+    const tour = await Tour.deleteTour(req.params.id);
+    res.status(204).json({
+      status: 'success',
+      data: tour,
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      message: error,
+    });
+  }
 };
--- server.js
+++ server.js
@@ -8,7 +8,6 @@ const DB = process.env.DATABASE;
 
 mongoose.connect(DB).then(() => console.log('DB connection successfully'));
 
-
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
